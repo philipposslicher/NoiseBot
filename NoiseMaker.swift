07426dab8b24863724733ec833973eb76d21ef35
Philippos Slicher <philipposslicher@gmail.com>
2015-11-30 11:51:46 +0400

    NOT USABLE: Starting to finish NoiseMaker class.

--- NoiseMaker.swift
+++ NoiseMaker.swift
@@ -9,9 +9,39 @@
 import Foundation
 import AVFoundation
 
-class NoiseMaker {
-    var sandPlayer: AVAudioPlayer?
-    var applausePlayer: AVAudioPlayer?
-    var monsterPlayer: AVAudioPlayer?
-    var bubblesPlayer: AVAudioPlayer?
-}
+class NoiseMaker {//
+    
+    private let sandPlayer: AVAudioPlayer?
+    private let hornPlayer: AVAudioPlayer?
+    private let XPPlayer: AVAudioPlayer?
+    private let LOLPlayer: AVAudioPlayer?
+    
+    func playSand() {
+        let url = NSBundle.mainBundle().pathForResource("sand", ofType:"wav")
+        let fileURL = NSURL(fileURLWithPath: url!)
+        sandPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
+        sandPlayer!.play()
+    }
+    
+    
+    func playHorn() {
+        let url = NSBundle.mainBundle().pathForResource("horn", ofType:"wav")
+        let fileURL = NSURL(fileURLWithPath: url!)
+        hornPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
+        hornPlayer!.play()
+    }
+    
+    func playXP() {
+        let url = NSBundle.mainBundle().pathForResource("xp", ofType:"wav")
+        let fileURL = NSURL(fileURLWithPath: url!)
+        XPPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
+        XPPlayer!.play()
+    }
+    
+    func playLOL() {
+        let url = NSBundle.mainBundle().pathForResource("lol", ofType:"wav")
+        let fileURL = NSURL(fileURLWithPath: url!)
+        LOLPlayer = try! AVAudioPlayer(contentsOfURL: fileURL)
+        LOLPlayer!.play()
+    }
+}//
--- NoiseMaker/ViewController.swift
+++ NoiseMaker/ViewController.swift
@@ -1,10 +1,6 @@
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {//
-
-    
-    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,35 +13,6 @@ class ViewController: UIViewController {//
     }
 
     
-
-    @IBAction func playSand(sender: UIButton) {
-        let url = NSBundle.mainBundle().pathForResource("sand", ofType:"wav")
-        let fileURL = NSURL(fileURLWithPath: url!)
-        player = try! AVAudioPlayer(contentsOfURL: fileURL)
-        player!.play()
-    }
-    
-    
-    @IBAction func playHorn(sender: UIButton) {
-        let url = NSBundle.mainBundle().pathForResource("horn", ofType:"wav")
-        let fileURL = NSURL(fileURLWithPath: url!)
-        player = try! AVAudioPlayer(contentsOfURL: fileURL)
-        player!.play()
-    }
-    
-    @IBAction func playXP(sender: UIButton) {
-        let url = NSBundle.mainBundle().pathForResource("xp", ofType:"wav")
-        let fileURL = NSURL(fileURLWithPath: url!)
-        player = try! AVAudioPlayer(contentsOfURL: fileURL)
-        player!.play()
-    }
-    
-    @IBAction func playLOL(sender: UIButton) {
-        let url = NSBundle.mainBundle().pathForResource("lol", ofType:"wav")
-        let fileURL = NSURL(fileURLWithPath: url!)
-        player = try! AVAudioPlayer(contentsOfURL: fileURL)
-        player!.play()
-    }
 
 }//
 
