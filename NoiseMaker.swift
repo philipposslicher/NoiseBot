7fe4845bae994911f5b6ac71749a3023623d8176
Philippos Slicher <philipposslicher@gmail.com>
2015-11-30 11:43:58 +0400

    Started making NoiseMaker class

---
+++ NoiseMaker.swift
@@ -0,0 +1,17 @@
+//
+//  NoiseMaker.swift
+//  NoiseMaker
+//
+//  Created by Philippos Slicher on 11/30/15.
+//  Copyright © 2015 Your School. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class NoiseMaker {
+    var sandPlayer: AVAudioPlayer?
+    var applausePlayer: AVAudioPlayer?
+    var monsterPlayer: AVAudioPlayer?
+    var bubblesPlayer: AVAudioPlayer?
+}
